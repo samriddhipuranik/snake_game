d479b54eccf048f01a49773df6414209875b6883
Samriddhi Puranik <samriddhi.18puranik@gmail.com>
2023-01-13 23:03:13 +0530

    commit message

--- js/index.js
+++ js/index.js
@@ -144,25 +144,21 @@ window.addEventListener('keydown',e=>{
 
     switch (e.key) {
         case "ArrowUp":
-             console.log("ArrowUp");
              inputDir.x= 0;
              inputDir.y= -1;
              break;
 
         case "ArrowDown":
-                console.log("ArrowDown");
                 inputDir.x= 0;
                 inputDir.y= 1;
                 break;
 
         case "ArrowLeft":
-                console.log("ArrowLeft");
                 inputDir.x= -1;
                 inputDir.y= 0;
                 break;
 
         case "ArrowRight":
-            console.log("ArrowRight");
             inputDir.x= 1;
             inputDir.y= 0;
             break;
